66ddf1676e74382aacec834fdc528e6bb6d91966
victor4107 <viktors1388@gmail.com>
2020-12-11 17:58:26 +0200

    42424242

---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest as ut
+import copy
+import matrix as pt
+
+
+class MyTest(ut.TestCase): #створення класу для реалізації тестів
+    def setUp(self):
+        self.mp=pt
+    def test_usage1(self):#створення функції для тесту
+        self.assertIsNot(self.mp.A, self.mp.transp(A))#використання команди self.assertIsNot() для порівняння матриці та транспонованої до неї
+    def test_usage2(self):#створення функції для тесту
+        self.assertIsNotNone(self.mp.det( A )) #використання команди self.assertIsNotNone() для перевірки значення дескримінанту
+    def test_usage3(self):#створення функції для тесту
+        self.assertIsNotNone(self.mp.matmult(A,B)) #використання команди self.assertIsNotNone для перевірки значення множення
+if __name__ == "__main__":
+    ut.main()#команда яка запускає всі тести із заданого модуля
