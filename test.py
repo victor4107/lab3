53e7d02e8bc406a940f2e9edff8b27602b8ecc89
victor4107 <viktors1388@gmail.com>
2020-12-11 20:35:55 +0200

    72444253745

--- test.py
+++ test.py
@@ -5,12 +5,17 @@ import matrix as pt
 
 class MyTest(ut.TestCase): #створення класу для реалізації тестів
     def setUp(self):
+        
+        self.mj=pt.B
         self.mp=pt.A
+        self.mt=pt.transp(pt.A)
+        self.mh=pt.det(pt.A)
+        self.mk=pt.matmult(pt.A,pt.B)
     def test_usage1(self):#створення функції для тесту
-        self.assertIsNot(self.mp.A, self.mp.transp(A))#використання команди self.assertIsNot() для порівняння матриці та транспонованої до неї
+        self.assertIsNot(self.mp, self.mt)#використання команди self.assertIsNot() для порівняння матриці та транспонованої до неї
     def test_usage2(self):#створення функції для тесту
-        self.assertIsNotNone(self.mp.det( A )) #використання команди self.assertIsNotNone() для перевірки значення дескримінанту
+        self.assertIsNotNone(self.mh) #використання команди self.assertIsNotNone() для перевірки значення дескримінанту
     def test_usage3(self):#створення функції для тесту
-        self.assertIsNotNone(self.mp.matmult(A,B)) #використання команди self.assertIsNotNone для перевірки значення множення
+        self.assertIsNotNone(self.mk) #використання команди self.assertIsNotNone для перевірки значення множення
 if __name__ == "__main__":
     ut.main()#команда яка запускає всі тести із заданого модуля
